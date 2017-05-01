89ce5d4990c8fab2fafe7ad855c0f1852c6c26bd
huifeng <sgrrzhf@qq.com>
2017-05-01 20:54:20 +0800

    删除SHT20类的address参数，添加README.mk

---
+++ SHT20/README.md
@@ -0,0 +1,33 @@
+---
+title: SHT20
+tags: python,lib,sensor
+grammar_cjkRuby: true
+---
+
+# 类SHT20
+## 初始化参数
+``scl_pin`` I2C的scl引脚序号，默认为16
+``sda_pin`` I2C的sda引脚序号，默认为17
+``clk_freq`` I2C的时钟频率，默认为100k
+
+## 函数
+`` get_temperature()``
+功能：获取温度数据
+返回：float型变量，温度
+
+``get_relative_humidity()``
+功能：获取相对湿度
+返回值：float型变量，相对湿度
+
+## 范例
+```
+>>>from sht20 import SHT20
+
+>>> sht_sensor = SHT20()
+>>> T = sht_sensor.get_temperature()
+>>> RH = sht_sensor.get_relative_humidity()
+>>> print('temperature: %f'%T)
+temperature: 26.423867
+>>> print('relative_humidity: %f'%RH)
+relative_humidity: 45.738740
+```
--- SHT20/sht20.py
+++ SHT20/sht20.py
@@ -1,13 +1,13 @@
 from machine import Pin, I2C
 from struct import unpack as unp
-from time import sleep_ms as delay_ms
+from time import sleep_ms
 
 # SHT20 default address
 SHT20_I2CADDR = 64
 
 # SHT20 Command
 TRI_T_MEASURE_NO_HOLD = b'\xf3'
-TRI_RH_MEASURE_NO_HOLD = b'\xf5' 
+TRI_RH_MEASURE_NO_HOLD = b'\xf5'
 READ_USER_REG = b'\xe7'
 WRITE_USER_REG = b'\xe6'
 SOFT_RESET = b'\xfe'
@@ -15,7 +15,7 @@ SOFT_RESET = b'\xfe'
 
 class SHT20(object):
 
-    def __init__(self, address=SHT20_I2CADDR, scl_pin=16, sda_pin=17, clk_freq=100000):
+    def __init__(self, scl_pin=16, sda_pin=17, clk_freq=100000):
         self._address = SHT20_I2CADDR
 
         pin_c = Pin(scl_pin)
@@ -24,7 +24,7 @@ class SHT20(object):
 
     def get_temperature(self):
         self._bus.writeto(self._address, TRI_T_MEASURE_NO_HOLD)
-        delay_ms(150)
+        sleep_ms(150)
         origin_data = self._bus.readfrom(self._address, 2)
         origin_value = unp('>h', origin_data)[0]
         value = -46.85 + 175.72 * (origin_value / 65536)
@@ -32,9 +32,8 @@ class SHT20(object):
 
     def get_relative_humidity(self):
         self._bus.writeto(self._address, TRI_RH_MEASURE_NO_HOLD)
-        delay_ms(150)
+        sleep_ms(150)
         origin_data = self._bus.readfrom(self._address, 2)
         origin_value = unp('>H', origin_data)[0]
         value = -6 + 125 * (origin_value / 65536)
         return value
-
